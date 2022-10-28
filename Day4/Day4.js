44a49fb30119ee12951a276837efef9eb1ad25dc
Kishor Chaudhary <kishorc2000@gmail.com>
2022-10-28 14:06:34 +0545

    Update

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -1,46 +1,29 @@
-class Box {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-    }
-    getTopBox() {
-        if (this.y === 0) return null
-        return new Box(this.x, this.y - 1)
-    }
-    getRightBox() {
-        if (this.x === 3) return null
-        return new Box(this.x + 1, this.y)
-    }
-    getBottomBox() {
-        if (this.y === 3) return null
-        return new Box(this.x, this.y + 1)
-    }
-    getLeftBox() {
-        if (this.x === 0) return null
-        return new Box(this.x - 1, this.y)
-    }
-    getNextdoorBoxes() {
-        return [
-            this.getTopBox(),
-            this.getRightBox(),
-            this.getBottomBox(),
-            this.getLeftBox(),
-        ].filter((box) => box !== null)
-    }
+let popped = 0;
 
-    getRandomNextdoorBox() {
-        const nextdoorBoxes = this.getNextdoorBoxes()
-        return nextdoorBoxes[Math.floor(Math.random() * nextdoorBoxes.length)]
+document.addEventListener('mouseover', function (e) {
+
+    if (e.target.className === "balloon") {
+
+        e.target.style.backgroundColor = "#19172e";
+        e.target.textContent = "🎈";
+        popped++;
+        removeEvent(e);
+        checkAllPopped();
     }
-}
-const swapBoxes = (grid, box1, box2) => {
-    const temp = grid[box1.y][box1.x]
-    grid[box1.y][box1.x] = grid[box2.y][box2.x]
-    grid[box2.y][box2.x] = temp
-}
-const isSolved = (grid) => {
-    return {
-        grid[0][0] === 1 &&
-            grid[0][1] === 2 &&
+});
+
+function removeEvent(e) {
+    e.target.removeEventListener('mouseover', function () {
+
+    })
+};
+
+function checkAllPopped() {
+    if (popped === 10) {
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
     }
-}
+};
